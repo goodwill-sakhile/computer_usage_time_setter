12de389c571b6a47a9cb6c5e43371a6426434bac
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2023-07-11 14:20:33 +0200

    computer commands script

---
+++ commands_script[1].py
@@ -0,0 +1,8 @@
+import os
+
+def shutDownTerminal():
+    os.system("shutdown /s /t 0")
+def restartTerminal():
+    os.system("shutdown /r /t 0")
+def sendTimerSettingOnTerminal(terminal_ip_address):
+    pass
