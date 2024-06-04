88b4e41fff1dd4430b0d2f6306cb9364c71fc48a
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2024-06-04 15:03:01 +0200

    Delete commands_script[1].py

--- commands_script[1].py
+++
@@ -1,8 +0,0 @@
-import os
-
-def shutDownTerminal():
-    os.system("shutdown /s /t 0")
-def restartTerminal():
-    os.system("shutdown /r /t 0")
-def sendTimerSettingOnTerminal(terminal_ip_address):
-    pass
